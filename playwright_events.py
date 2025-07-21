995db579e9212d72435dd18fbf7b6cc723c07222
hooehu <imalyshev03@gmail.com>
2025-07-21 21:55:00 +0300

    in progress

---
+++ playwright_events.py
@@ -0,0 +1,4 @@
+from playwright.sync_api import sync_playwright, expect
+
+with sync_playwright() as playwright:
+    browser
--- playwright_keyboard.py
+++ playwright_keyboard.py
@@ -2,16 +2,19 @@ from playwright.sync_api import sync_playwright, expect
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
-
     page = browser.new_page()
     page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login')
-    email_input = page.locator('//div[@data-testid="login-form-email-input"]//div//input')
-    # Курсор на веб элемент
+
+    email_input = page.get_by_test_id('login-form-email-input').locator('input')
+
+    # Ставим фокус на веб-элемент
     email_input.focus()
 
-    for char in 'user.name@gmail.com':
-        page.keyboard.type(char, delay=180)
+    for char in 'user@gmail.com':
+        # Вводим каждый символ по отдельности. Задержка между вводом символов для наглядности
+        page.keyboard.type(char, delay=190)
 
-    page.keyboard.press("ControlOrMeta+A")
+    # Применяем Cmd+A для выделения ввденного текста
+    page.keyboard.press('ControlOrMeta+A')
 
     page.wait_for_timeout(2000)
