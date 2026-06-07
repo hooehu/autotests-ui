6d2e3bbac937a0ca13fb451ac6c0d629e3226b89
hooehu <imalyshev03@gmail.com>
2026-06-07 21:16:58 +0300

    Практикуемся в реализации PageObject. Практикуемся в написании автотестов с использованием POM

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,5 +1,10 @@
 import pytest
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import sync_playwright, expect, Page
+
+from pages.courses_list_page import CoursesListPage
+from pages.create_course_page import CreateCoursePage
+
+
 @pytest.mark.courses
 @pytest.mark.regression
 def test_empty_courses_list(chromium_page_with_state):
@@ -19,6 +24,32 @@ def test_empty_courses_list(chromium_page_with_state):
     expect(title_empty_list).to_have_text("There is no results")
     expect(text_description_empty_list).to_be_visible()
     expect(text_description_empty_list).to_have_text("Results from the load test pipeline will be displayed here")
+
+@pytest.mark.courses
+@pytest.mark.regression
+def test_create_course(courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
+    create_course_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
+    create_course_page.check_visible_create_course_title()
+    create_course_page.check_disabled_create_course_button()
+    create_course_page.check_visible_image_preview_empty_view()
+
+    # Не будет ли избыточно передавть: is_image_uploaded=False ?
+    create_course_page.check_visible_image_upload_view(is_image_uploaded=False)
+    create_course_page.check_visible_create_course_form('', '', '', '0', '0')
+    create_course_page.check_visible_exercises_title()
+    create_course_page.check_visible_create_exercise_button()
+    create_course_page.check_visible_exercises_empty_view()
+    create_course_page.upload_preview_image('./testdata/files/image.png')
+    create_course_page.check_visible_image_upload_view(is_image_uploaded=True)
+    create_course_page.fill_create_course_form(
+        'Playwright', '2 weeks', 'Playwright', '100', '10'
+    )
+    create_course_page.click_create_course_button()
+
+    # Проверяем созданный курс
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_course_card(0, 'Playwright', '100', '10', '2 weeks')
 
 
 
