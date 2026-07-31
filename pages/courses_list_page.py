0e0a12ec6bbe45650d8a7db1b0359b797d5069ee
hooehu <imalyshev03@gmail.com>
2026-07-31 20:17:35 +0300

    PageComponent practice

--- pages/courses_list_page.py
+++ pages/courses_list_page.py
@@ -1,6 +1,7 @@
 from playwright.sync_api import Page, expect
 
 from components.coursers.course_view_component import CourseViewComponent
+from components.coursers.toolbar_component import ToolbarComponent
 from components.navigation.navbar_component import NavbarComponent
 from components.navigation.sidebar_component import SidebarComponent
 from components.views.empty_view_component import EmptyViewComponent
@@ -16,13 +17,8 @@ class CoursesListPage(BasePage):
         self.navbar = NavbarComponent(page)
         self.empty_view = EmptyViewComponent(page, identifier='courses-list')
         self.course_view = CourseViewComponent(page)
+        self.toolbar_view = ToolbarComponent(page)
 
-        self.courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
-        self.create_course_button = page.get_by_test_id('courses-list-toolbar-create-course-button')
-
-    def check_visible_courses_title(self):
-        expect(self.courses_title).to_be_visible()
-        expect(self.courses_title).to_have_text('Courses')
 
     def check_visible_empty_view(self):
         self.empty_view.check_visible(
@@ -30,9 +26,4 @@ class CoursesListPage(BasePage):
             description='Results from the load test pipeline will be displayed here'
         )
 
-    def check_visible_create_course_button(self):
-        expect(self.create_course_button).to_be_visible()
-
-    def click_create_course_button(self):
-        self.create_course_button.click()
 
